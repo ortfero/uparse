087aee7d0d99764e4923ae40a7fb8d148c1f838f
Andrei Ilin <ortfero@gmail.com>
2024-04-10 17:46:30 +0300

    Parsing null strings

--- include/uparse/uparse.hpp
+++ include/uparse/uparse.hpp
@@ -1,5 +1,5 @@
 // This file is part of uparse library
-// Copyright 2022 Andrei Ilin <ortfero@gmail.com>
+// Copyright 2022-2024 Andrei Ilin <ortfero@gmail.com>
 // SPDX-License-Identifier: MIT
 
 
@@ -18,20 +18,33 @@ namespace uparse {
     namespace detail {
     
         template<typename T> std::optional<T> parse_number(std::string_view s) noexcept {
-            T value;
+			T value;
             auto const parsed = std::from_chars(s.data(), s.data() + s.size(), value);
             if(parsed.ec != std::errc{})
                 return std::nullopt;
             return {value};
         }
+		
+		
+		template<typename T> std::optional<T> parse_number(char const* s) noexcept {
+			if(!s)
+				return std::nullopt;
+			return parse_number<T>(std::string_view{s});
+		}
         
     } // namespace detail
     
     
     template<typename T> std::optional<T> parse(std::string_view) noexcept = delete;
+	template<typename T> std::optional<T> parse(char const*) noexcept = delete;
     
     
     template<> std::optional<std::int32_t> parse(std::string_view s) noexcept {
+        return detail::parse_number<std::int32_t>(s);
+    }
+	
+	
+	template<> std::optional<std::int32_t> parse(char const* s) noexcept {
         return detail::parse_number<std::int32_t>(s);
     }
     
@@ -39,14 +52,28 @@ namespace uparse {
     template<> std::optional<std::uint32_t> parse(std::string_view s) noexcept {
         return detail::parse_number<std::uint32_t>(s);
     }
+	
+	
+	template<> std::optional<std::uint32_t> parse(char const* s) noexcept {
+        return detail::parse_number<std::uint32_t>(s);
+    }
     
     
     template<> std::optional<std::int64_t> parse(std::string_view s) noexcept {
         return detail::parse_number<std::int64_t>(s);
     }
+	
+	template<> std::optional<std::int64_t> parse(char const* s) noexcept {
+        return detail::parse_number<std::int64_t>(s);
+    }
     
     
     template<> std::optional<std::uint64_t> parse(std::string_view s) noexcept {
+        return detail::parse_number<std::uint64_t>(s);
+    }
+	
+	
+	template<> std::optional<std::uint64_t> parse(char const* s) noexcept {
         return detail::parse_number<std::uint64_t>(s);
     }
     
@@ -58,6 +85,21 @@ namespace uparse {
         char* endptr;
         auto const value = std::strtod(s.data(), &endptr);
         if(endptr != s.data() + s.size())
+            return std::nullopt;
+        return {value};
+#endif
+    }
+	
+	
+	template<> std::optional<double> parse(char const* s) noexcept {
+#ifdef _MSC_VER
+        return detail::parse_number<double>(s);
+#else
+		if(s == nullptr)
+			return std::nullopt;
+		char* endptr;
+        auto const value = std::strtod(s, &endptr);
+        if(*endptr != '\0')
             return std::nullopt;
         return {value};
 #endif
